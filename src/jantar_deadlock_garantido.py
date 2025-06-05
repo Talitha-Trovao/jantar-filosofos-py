e190a97843dd2fada139b3087ed4b3e6ced9049c
talitha.trovao <talitha.trovao@gmail.com>
2025-06-05 13:36:38 -0300

    inserindo resolução com deadlock

---
+++ src/jantar_deadlock_garantido.py
@@ -0,0 +1,43 @@
+import threading #habilita multiplos threads para execuçãoparaela no pthon
+import time
+
+n_filo = 5 #numero de filososos no teste
+garfos = [threading.Lock() for _ in range(n_filo)]
+pegou_primeiro_garfo = threading.Semaphore(0)
+segundo_garfo_livre = threading.Semaphore(0)
+
+def pensar(id):
+    print(f"Filósofo {id} está pensando...")
+    time.sleep(0.5) #aguarda para pensar
+
+def comer(id):
+    print(f"Filósofo {id} está comendo.")
+    time.sleep(0.5) #tempo de comer
+
+def filosofo(id):
+    garfo_esq = id
+    garfo_dir = (id + 1) % n_filo
+    while True:
+        pensar(id)
+        print(f"Filósofo {id} com fome e tentando pegar garfo {garfo_esq} (na esquerda)")
+        garfos[garfo_esq].acquire() #sincroniza
+        print(f"Filósofo {id} pegou garfo {garfo_esq} (na esquerda)")
+        pegou_primeiro_garfo.release() #libera garfo
+        for _ in range(n_filo):
+            segundo_garfo_livre.acquire()
+        for _ in range(n_filo):
+            segundo_garfo_livre.release()
+        print(f"Filósofo {id} tentando pegar garfo {garfo_dir} (na direita)")
+        garfos[garfo_dir].acquire()  # Aqui pode ocorrer deadlock já que os 2 garfos pode não estar disponiveis
+        print(f"Filósofo {id} pegou garfo {garfo_dir} (na direita)")
+        comer(id)
+        print(f"Filósofo {id} deixou o garfo {garfo_esq} (na esquerda)")
+        garfos[garfo_esq].release()
+        print(f"Filósofo {id} deixou o garfo {garfo_dir} (na direita)")
+        garfos[garfo_dir].release()
+
+threads = [] #inicia livre
+for i in range(n_filo):
+    t = threading.Thread(target=filosofo, args=(i,)) #uma thread para cada filsofo no ciclo
+    t.start()
+    threads.append(t)
